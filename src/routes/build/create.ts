4c97fa3706235cdadd896081529e7b1115e06763
acrylic-style <me@acrylicstyle.xyz>
2022-06-15 00:19:37 +0900

    Split at API side

--- src/routes/build/create.ts
+++ src/routes/build/create.ts
@@ -15,14 +15,13 @@ export default async (req: Request, res: Response) => {
   if (req.headers.authorization !== `Bearer ${process.env.SECRET_KEY}`) {
     return res.status(401).send({ error: 'unauthorized' })
   }
-  const pOwner = toStringOrNull(req.query['owner'])
   const pRepo = toStringOrNull(req.query['repo'])
   const pCommit = toStringOrNull(req.query['commit'])
   const pVersionGroup = toStringOrNull(req.query['versionGroup'])
   const pVersion = toStringOrNull(req.query['version'])
   const pBranch = toStringOrNull(req.query['branch'])
-  if (!pOwner ||
-    !pRepo ||
+  if (!pRepo ||
+    !pRepo.includes('/') ||
     !pCommit ||
     !pVersionGroup ||
     !pVersion ||
@@ -31,6 +30,8 @@ export default async (req: Request, res: Response) => {
     !VERSION_REGEX.test(pVersion)) {
     return res.status(400).send({ error: 'missing or invalid parameters' })
   }
+  const repo = pRepo.split('/')[0]
+  const owner = pRepo.split('/')[1]
   const dottedVersionGroup = pVersionGroup.replace('_', '.')
   const experimental = pBranch !== 'main' && pBranch !== 'master' && !pBranch.startsWith('ver/')
   const project = await res.locals.getProject()
@@ -61,28 +62,37 @@ export default async (req: Request, res: Response) => {
   const veryLongQueryWithSubQueries = 'SELECT `sha` FROM `build_changes` WHERE `build_id` = (SELECT `id` FROM `builds` WHERE `version_id` IN (SELECT `id` FROM `versions` WHERE `version_group_id` = ?) ORDER BY `build_number` DESC LIMIT 1)'
   const lastCommits = await findAll(veryLongQueryWithSubQueries, versionGroup.id).then(res => res.map(value => value as string))
   const buildId = await findOne('INSERT INTO `builds` (`version_id`, `build_number`, `experimental`) VALUES (?, ?, ?)', version.id, buildNumber, experimental) as number
-  const handmadeDownloadUrl = `https://github.com/${pOwner}/${pRepo}/releases/download/${pVersion}.${buildNumber}/blueberry-${pVersion}.${buildNumber}-installer.jar`
+  const handmadeDownloadUrl = `https://github.com/${owner}/${repo}/releases/download/${pVersion}.${buildNumber}/blueberry-${pVersion}.${buildNumber}-installer.jar`
   await query('INSERT INTO `build_files` (`build_id`, `type`, `download_url`) VALUES (?, ?, ?)', buildId, 'universal-installer', handmadeDownloadUrl)
   const getCommitResult = await octokit.rest.repos.getCommit({
     ref: pCommit,
-    owner: pOwner,
-    repo: pRepo,
+    owner: owner,
+    repo: repo,
   })
   if (getCommitResult.status !== 200) {
     return res.status(500).send({ error: 'internal server error', message: 'failed to calculate changes' })
   }
-  await query('INSERT INTO `build_changes` (`build_id`, `sha`, `description`) VALUES (?, ?, ?)', buildId, getCommitResult.data.sha, getCommitResult.data.commit.message)
-  const compareResult = await octokit.rest.repos.compareCommits({
-    base: lastCommits[0],
-    head: pCommit,
-    owner: pOwner,
-    repo: pRepo,
-  })
-  if (compareResult.status !== 200) {
-    return res.status(500).send({ error: 'internal server error', message: 'failed to calculate changes' })
-  }
-  for (const commit of compareResult.data.commits) {
-    await query('INSERT INTO `build_changes` (`build_id`, `sha`, `description`) VALUES (?, ?, ?)', buildId, commit.sha, commit.commit.message)
+  try {
+    if (lastCommits.length > 0) {
+      const compareResult = await octokit.rest.repos.compareCommits({
+        base: lastCommits[0],
+        head: pCommit,
+        owner: owner,
+        repo: repo,
+      })
+      if (compareResult.status !== 200) {
+        return res.status(500).send({ error: 'internal server error', message: 'failed to calculate changes' })
+      }
+      for (const commit of compareResult.data.commits) {
+        if (lastCommits.includes(commit.sha) || pCommit === commit.sha) {
+          continue
+        }
+        await query('INSERT INTO `build_changes` (`build_id`, `sha`, `description`) VALUES (?, ?, ?)', buildId, commit.sha, commit.commit.message)
+      }
+    }
+  } finally {
+    // try to insert at the end
+    await query('INSERT INTO `build_changes` (`build_id`, `sha`, `description`) VALUES (?, ?, ?)', buildId, getCommitResult.data.sha, getCommitResult.data.commit.message)
   }
   res.status(200).send({ message: 'accepted' })
 }
