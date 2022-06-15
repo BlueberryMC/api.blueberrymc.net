ffbe469d237dfeaad652f229c570937ef1950b18
acrylic-style <me@acrylicstyle.xyz>
2022-06-15 17:14:13 +0900

    (Really) Fix picking up wrong version group

--- src/routes/version_group/index.ts
+++ src/routes/version_group/index.ts
@@ -8,7 +8,7 @@ router.use(async (req, res: Response, next) => {
   const versionGroupIdOrName = String(req.params.versionGroup)
   res.locals.getVersionGroup = async () => {
     const projectId = parseInt(versionGroupIdOrName, 10)
-    if (!isNaN(projectId) && projectId > 0 && !String(req.params.versionGroup).includes('.')) {
+    if (!isNaN(projectId) && projectId > 0 && !versionGroupIdOrName.includes('.')) {
       const versionGroup = VersionGroup.findById(projectId)
       if (versionGroup) {
         return versionGroup
